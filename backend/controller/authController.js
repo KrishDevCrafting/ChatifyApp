569ff05de0743888d804402ea8932f774cac3904
KrishDevCrafting <krishkumer150@gmail.com>
2026-08-20 13:39:49 +0530

    updated

---
+++ backend/controller/authController.js
@@ -0,0 +1,9 @@
+import pool from "../Config/db";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const registerUser = async () =>{
+    const {username , email , password} = req.body;
+    
+}
+
