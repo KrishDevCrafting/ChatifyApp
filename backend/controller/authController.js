2d27ffef4f3fad5518c903abdd70ba9e1e78c9b0
KrishDevCrafting <krishkumer150@gmail.com>
2026-08-25 23:16:54 +0530

    updated

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -31,22 +31,62 @@ const registerUser = async (req, res) => {
       message: "User registered successfully!",
       userId: result.insertId,
     });
-
   } catch (error) {
     console.error("Error in registerUser:", error);
     return res.status(500).json({ message: "Internal server error" });
   }
 };
 
+// 2. Login users
 
+const loginUser = async (eq, res) => {
+  try {
+    const { email, password } = req.body;
 
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and password are required" });
+    }
 
+    const user = await FindbyEmail(email);
 
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email and password" });
+    }
 
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
 
+    if (!isPasswordMatch) {
+      return res.status(400).json({ message: "Invalid email or passwrod" });
+    }
 
+    const token = jwt.sign(
+      {
+        id: user.id,
+        username: user.username,
+      },
 
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      },
+    );
+    return res.status(200).json({
+      message: "Login successful!",
+      token,
+      user: {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error("Error in loginUser:", error);
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
 
-
-
-export { registerUser };
+export { registerUser, loginUser };
