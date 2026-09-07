f86cbc467130ae7060f145b1b34f34ff6252d02a
Krish <krishkumer150@gmail.com>
2026-09-07 23:39:03 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,60 +1,19 @@
-import {BrowserRouter, Routes,Route} from "react-router-dom"
-import Login from "./Login"
-import HomePage from "./home/Homepage"
+import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
+
+import Login from "./Login.jsx";
+import HomePage from "./home/Homepage";
 
 function App() {
-
-
-
-
-
   return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login"/>} />
+        <Route path="/login" element={<Login />} />
 
-
-<>
-
-
-<BrowserRouter>
-
-<Routes>
-
-
-
-
-
-
-  <Route path="/" element={<HomePage/>} />
-   <Route path="/login" element={<Login/>} />
-    {/* <Route path="/register" element={} /> */}
-</Routes>
-
-
-
-
-
-
-</BrowserRouter>
-
-
-
-
-
-
-
-
-</>
-
-  )
-
-
-
-
-
-
-
-
-
-
+        {/* <Route path="/register" element={<Register />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
